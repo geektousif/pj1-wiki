4457f691d78413214579e7c020e91a17304f4175
geektousif <syedmdtousif.2002@gmail.com>
2020-10-13 20:23:33 +0530

    Final Commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wikipage, name="wikipage"),
-    path("search", views.new_search, name="search")
+    path("search", views.new_search, name="search"),
+    path("add_entry", views.add_view, name='add_entry'),
+    path("edit_entry/<str:title>", views.edit_entry, name='edit'),
+    path("random", views.random_wiki, name="random_wiki")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import Markdown
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django import forms
+from django.views.generic.edit import UpdateView
+import random
 
 from . import util
 
@@ -28,9 +30,71 @@ def new_search(request):
 
 
     for entry in entries:
-        if entry.lower() == searches.lower():
-            search_result.append(searches)
+        if searches.lower() in entry.lower():
+            search_result.append(entry)
         else:
-            return HttpResponse("No Matches")
+            pass
+
 
     return render(request, "encyclopedia/search.html", {"search_result" : search_result})
+
+class AddForm(forms.Form):
+    title = forms.CharField(max_length=20)
+    content = forms.CharField(widget=forms.Textarea)
+
+def add_view(request):
+    entries = util.list_entries()
+    entries = [entry.lower() for entry in entries]
+    errors = []
+    if request.method == 'POST':
+        form = AddForm(request.POST)
+        title = form['title'].value()
+
+        if title.lower() in entries:
+            error = "Already Available Data"
+            errors.append(error)
+
+        else:
+            if form.is_valid():
+                title = form.cleaned_data['title']
+                content = form.cleaned_data['content']
+                util.save_entry(title, content)
+            else:
+                form = AddForm()
+
+            return HttpResponseRedirect('/')
+    else:
+        form = AddForm()
+
+    return render(request, "encyclopedia/add_new.html", {"form" : form, "errors": errors})
+
+class EditForm(forms.Form):
+    content = forms.CharField(widget=forms.Textarea)
+
+def edit_entry(request, title):
+    entry = util.get_entry(title)
+    if request.method == 'GET':
+        form = EditForm(initial={'content' : entry})
+
+    if request.method == 'POST':
+        form = EditForm(request.POST)
+        if form.is_valid():
+            content = form.cleaned_data['content']
+            util.save_entry(title, content)
+        return HttpResponseRedirect('/')
+
+    return render(request, "encyclopedia/edit.html", {"form":form, "title":title})
+
+def random_wiki(request):
+    entries = util.list_entries()
+    number = len(entries) - 1
+    rand_entry_index = random.randint(0, number)
+    random_entry = entries[rand_entry_index]
+    random_content = util.get_entry(random_entry)
+    markdowner = Markdown()
+    md_entry = markdowner.convert(random_content)
+
+    return render(request, "encyclopedia/wikipage.html", {
+        "content": md_entry,
+        "wikientry": random_entry
+        })
